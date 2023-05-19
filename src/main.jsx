3860dadca715204a0540d7c9e60133eee0697fd9
unknown <muttakinahmed871@gmail.com>
2023-05-19 14:45:32 +0600

    basic layout setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+ import './index.css'
 
 import {
-  createBrowserRouter,
+   
   RouterProvider,
 } from "react-router-dom";
-import Home from './pages/Home/Home/Home.jsx';
+import router from './Routes/Routes';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<Home></Home> ,
-  },
-]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
