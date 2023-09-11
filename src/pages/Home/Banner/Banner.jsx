6005472054899e8e683cf914227866817b703da6
unknown <muttakinahmed871@gmail.com>
2023-09-11 19:17:46 +0600

    update banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -4,15 +4,9 @@ import img3 from "../../../assets/football.png";
 import img4 from "../../../assets/MiniBowlingPins.jpg";
 const Banner = () => {
   return (
-    <div className="carousel w-full h-[450px]">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          style={{
-            height: "100%",
-          }}
-          src={img1}
-          className="w-full"
-        />
+    <div className="carousel w-full max-h-[450px]">
+      <div id="slide1" className="carousel-item relative w-full h-[450px]">
+        <img src={img1} className="w-full h-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide4" className="btn btn-circle">
             ❮
@@ -22,8 +16,8 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img style={{ height: "100%" }} src={img2} className="w-full" />
+      <div id="slide2" className="carousel-item relative w-full h-[450px]">
+        <img src={img2} className="w-full h-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide1" className="btn btn-circle">
             ❮
@@ -33,8 +27,8 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img style={{ height: "100%" }} src={img3} className="w-full" />
+      <div id="slide3" className="carousel-item relative w-full h-[450px]">
+        <img src={img3} className="w-full h-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide2" className="btn btn-circle">
             ❮
@@ -44,8 +38,8 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img style={{ height: "100%" }} src={img4} className="w-full" />
+      <div id="slide4" className="carousel-item relative w-full h-[450px]">
+        <img src={img4} className="w-full h-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide3" className="btn btn-circle">
             ❮
